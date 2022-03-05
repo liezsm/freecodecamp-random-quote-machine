b105004650f70549b561db3d54e374e912594e28
liezsm <liez.dev@gmail.com>
2022-03-05 14:07:18 +0800

    Delete console log and added the author

--- src/Components/Quotebox.js
+++ src/Components/Quotebox.js
@@ -34,15 +34,12 @@ class Quotebox extends React.Component {
         const quotes = await fetch('https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json');
         const data = await quotes.json()
       
-        // const { id, advice} = data.slip;
-
-       console.log(data.quotes)
-       console.log(data.quotes.length)
+    //    console.log(data.quotes.length)
        const random = Math.floor( Math.random() * data.quotes.length)
-       console.log(random)
        console.log(data.quotes[random])
-
-       document.getElementById('text').innerText = `${data.quotes[random].quote}`
+        const {quote, author} = data.quotes[random]
+       document.getElementById('text').innerText = `${quote}`
+       document.getElementById('author').innerText = `${author}`
     //    console.log(id, advice)
 
         
